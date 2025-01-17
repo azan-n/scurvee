10eb26672bd639cd759472e2c65b56fd03c5af73
Azan <work@azan-n.com>
2025-01-18 03:33:59 +0530

    feat: chuck thingy

--- src/components/Chuck.tsx
+++ src/components/Chuck.tsx
@@ -22,11 +22,18 @@ const ChucKPlayer: React.FC = () => {
 
     const run = async () => {
         if (chuck && !isPlaying) {
+            chuck.setIntArray("mel", [0,2,3,1,4,2,6,3,4,4])
             // Run the ChucK code to produce sound if it's not already playing
             setIsPlaying(await chuck.runCode(`
-        SinOsc sin => dac;
-        440 => sin.freq;
-        1::week => now;
+global int mel[10]; //sequence data
+StifKarp inst => dac;
+[0,2,4,5,7,9,11,12] @=> int major[]; //major scale
+
+for (0=>int i; true ; i++) { //infinite loop
+  Std.mtof( 48 + major[mel[i%mel.cap()]] ) => inst.freq; //set the note
+  inst.noteOn( 0.5 ); //play a note at half volume
+  300::ms => now; //compute audio for 0.3 sec
+}
       `));
         }
     };
